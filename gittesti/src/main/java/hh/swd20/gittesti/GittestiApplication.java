f9272be86bb8d77d788d9ddc9d7d577b482e3473
sealittlehope <meri.toivonen@gmail.com>
2019-09-02 10:14:42 +0300

    first commit

---
+++ gittesti/src/main/java/hh/swd20/gittesti/GittestiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.gittesti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittestiApplication.class, args);
+	}
+
+}
